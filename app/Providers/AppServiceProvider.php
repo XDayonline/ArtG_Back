3da7414fe4c96465274b4af4bcb1a711233a46a3
Gianni <ganni.scantamburlo@ynov.com>
2020-04-07 11:21:28 +0200

    url fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot(UrlGenerator $url)
+    {
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+    }
+
     /**
      * Register any application services.
      *
@@ -13,16 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }

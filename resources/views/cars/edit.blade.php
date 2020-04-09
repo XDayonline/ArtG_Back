421c1540e8a3699d31f06a9de7a8b4f2fc0b920e
Gianni <ganni.scantamburlo@ynov.com>
2020-04-09 20:11:49 +0200

    img

--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -11,10 +11,26 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="form-group">
+                    <strong>Modèle</strong>
+                    <input type="text" name="name" class="form-control" placeholder="Entrez un modèle"
+                           value="{{ $car->name }}">
+                    <span class="text-danger">{{ $errors->first('name') }}</span>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group">
                     <strong>Marque</strong>
                     <input type="text" name="marque" class="form-control" placeholder="Entrez une marque"
                            value="{{ $car->marque }}">
                     <span class="text-danger">{{ $errors->first('marque') }}</span>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="form-group">
+                    <strong>Image</strong>
+                    <input type="text" name="urlimg" class="form-control" placeholder="Entrez l'url de l'image"
+                           value="{{ $car->urlimg }}">
+                    <span class="text-danger">{{ $errors->first('urlimg') }}</span>
                 </div>
             </div>
             <div class="col-md-12">
